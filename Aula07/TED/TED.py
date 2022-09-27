b88780369680446e3ca0285a4cae3b2d92910aab
Gonçalo Gabriel <112342553+GoncaloGabriel@users.noreply.github.com>
2022-09-26 23:11:42 -0300

    Update TED.py

--- Aula07/TED/TED.py
+++ Aula07/TED/TED.py
@@ -25,6 +25,7 @@ else: # Calculando com o valor normal
     '''Problema 2 : Ler o ano atual e o ano de nascimento de uma pessoa. Escrever uma mensagem que diga
      se ela poderá ou não votar este ano (não é necessário considerar o mês em que a pessoa nasceu).
     '''
+    
 ano_atual = int(input("Digite o ano atual: "))
 ano_nascimento = int(input("Digite o seu ano de nascimento: "))
 
