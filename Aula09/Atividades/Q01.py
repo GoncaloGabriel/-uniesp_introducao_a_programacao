ee3b46246a56288fb930144a571bea8a9f7c7032
Gonçalo Gabriel <112342553+GoncaloGabriel@users.noreply.github.com>
2022-10-11 20:11:44 -0300

    Update Q01.py

--- Aula09/Atividades/Q01.py
+++ Aula09/Atividades/Q01.py
@@ -3,10 +3,10 @@
 # Você deverá ter chaves como primeiro_nome, sobrenome, idade e cidade.
 # Por fim, mostre cada informação armazenada em seu dicionário.
 
-dados = {"primeiro_nome": "Willian", "sobrenome": "Pereira", "idade": 26, "cidade": "Starkville"}
+dados = {"primeiro_nome": "Luiz", "sobrenome": "Gonzaga", "idade": 27, "cidade": "João Pessoa"}
 print(dados.keys())
 print(dados.values())
 print(dados.items())
 
-for k,v in dados.items():
-    print(f"A chave e o valor deste item são: {k} e {v}.")
+for t,g in dados.items():
+    print(f"A chave e o valor deste item são: {t} e {g}.")
